75eb9f33d203d63b5e676bbc8b04487556d41995
brocha05 <brocha_05@hotmail.com>
2022-10-12 19:12:07 -0500

    Taskform has been added to handle add new todos

--- imports/ui/App.jsx
+++ imports/ui/App.jsx
@@ -2,13 +2,15 @@ import React from 'react';
 import { Tasks } from './Tasks';
 import { TasksCollection } from '/imports/api/TasksCollection';
 import { useTracker } from 'meteor/react-meteor-data';
+import { TaskForm } from './TaskForm';
 
 export const App = () => {
-  const tasks = useTracker(() => TasksCollection.find({}).fetch());
+  const tasks = useTracker(() => TasksCollection.find({}, { sort: { createdAt: -1 } }).fetch());
   
   return (
     <div>
-      <h1>Hello world from Meteor!</h1>
+      <h1>Todo List</h1>
+      <TaskForm/>
       <Tasks tasks={tasks}/>
     </div>
   )
